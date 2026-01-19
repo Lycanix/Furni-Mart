c810fb69a302d1d4a1c6daec6abdb2f40eee7949
Rama <syahrul.rama33@gmail.com>
2026-01-19 12:24:39 +0700

    feat: add navbar template

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,22 @@
-landingPage();
+const routes = {
+	landing: landingPage,
+	// product: productPage,
+	// wishlist,
+	// about,
+	// profile,
+	// cart,
+};
+
+function navigate(page) {
+	const handler = routes[page];
+	if (!handler) return;
+
+	handler();
+}
+
+function initApp() {
+	navbar(navigate);
+	navigate('landing');
+}
+
+initApp();
